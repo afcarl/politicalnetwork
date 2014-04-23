97e2fb143e69f16b6f7fed2d2cb71ac22e75e300
Christian <chr@nygaardc.com>
2014-04-23 15:54:44 +0200

    now classifying tweets
    added support for classifying tweets according to political party

--- classify_tweets.py
+++ classify_tweets.py
@@ -3,9 +3,68 @@ import re
 
 def getwords(document):
     splitter = re.compile('\\W*')
-
-    words = [s.lower() for s in splitter.split(document) if len(s) > 3 and len(s) < 20 ]
-
+    words = [s.lower() for s in splitter.split(document) if len(s) > 3 and len(s) < 20]
     return dict([(w,1) for w in words])
 
-cl = classifier(getwords)
+cl = classifier.naivebayes(getwords)
+
+# import tweets
+tweets_file = open('tweets.tsv', 'r')
+
+# count lines
+line_count = 1
+for line in tweets_file:
+    line_count += 1
+
+tweets_file.seek(0)
+
+train_range =range(int(0.5 * line_count))
+test_range = range(train_range[-1]+1, line_count)
+
+# train classifier
+for l in train_range:
+    data = tweets_file.readline().split('\t')
+    try:
+        tweet_text = data[-1]
+        party = data[2]
+        cl.train(tweet_text, party)
+    except:
+        continue
+
+# classify remaining data
+correct = 0
+unknown = 0
+for m in test_range:
+    data = tweets_file.readline().split('\t')
+    try:
+        tweet_text = data[-1]
+        party = data[2]
+        guess = cl.classify(tweet_text, default='unknown')
+        if guess == party: correct += 1
+        elif guess == 'unknown': unknown += 1
+    except:
+        continue
+
+tweets_file.close()
+
+precision = float(correct)/len(test_range)
+
+# find combinations of features and categories
+# so that we can see which words belong to D or R
+word_combinations = cl.fcombi
+
+# top words in classifier
+
+items = ((word, party, value) for word in word_combinations for party, value in word_combinations[word].items())
+ordered = sorted(items, key=lambda x: x[-1], reverse=True)
+
+democrats = {}
+republicans={}
+for word, party, value in ordered:
+    if party == 'D':
+        democrats[word] = value
+    elif party == 'R':
+        republicans[word] = value
+
+democrats_top25 = sorted(democrats.items(), key=lambda x: x[1], reverse=True)[:50]
+republicans_top25 = sorted(republicans.items(), key=lambda x: x[1], reverse=True)[:50]
--- download_tweets.py
+++ download_tweets.py
@@ -1,4 +1,7 @@
 from twython import Twython
+import re
+import requests
+import time
 
 # Import Authenticate Info
 from auth import *
@@ -12,17 +15,17 @@ file = open("accounts.tsv", "rb")
 
 accounts = {}
 for line in file:
-	parts = line[:-1].split("\t")
-	name = parts[0]
-	state = parts[1]
-	chamber = parts[2]
-	party = parts[3]
-	account_name = parts[4]
-	accounts[account_name] = {
-				'state':state ,
-				'full_name': name,
-				'chamber':chamber,
-				'party': party }
+    parts = line[:-1].split("\t")
+    name = parts[0]
+    state = parts[1]
+    chamber = parts[2]
+    party = parts[3]
+    account_name = parts[4]
+    accounts[account_name] = {
+                'state':state ,
+                'full_name': name,
+                'chamber':chamber,
+                'party': party }
 
 file.close()
 
@@ -31,27 +34,40 @@ file = open('tweets.tsv', 'w')
 
 count = 0 # remove this
 for user in accounts:
-	count += 1 # remove this
-	if count % 150 == 0:
-		time.sleep(15*60)
-    	try:
-        	tweets = twitter.get_user_timeline(screen_name=user, count=200)
-    	except:
-        	continue
-   	for tweet in tweets:
-        	tweet_txt = tweet['text']
-		tweet_time = tweet['created_at']
-		utc_offset = str(tweet['user']['utc_offset'])
-		mentions = [ m['screen_name'] for m in tweet['entities']['user_mentions']]
-        	line = user + "\t" 
-		line += accounts[user]['state'] + '\t' 
-		line += accounts[user]['party'] + "\t" 
-		line += accounts[user]['chamber'] + "\t"
-		line += ",".join(mentions) + "\t"
-		line += tweet_time + "\t"
-		line += utc_offset + "\t"
-		line += tweet_txt + '\n'
-        	file.write(line.encode("UTF-8"))
-	
+    timea = time.time()
+    print "fetching",user
+    count += 1 # remove this
+    if count % 150 == 0:
+        time.sleep(15*60)
+    try:
+        tweets = twitter.get_user_timeline(screen_name=user, count=200)
+    except:
+        continue
+    for tweet in tweets:
+        tweet_txt = tweet['text']
+        tweet_time = tweet['created_at']
+        # get real url for all mentioned urls
+        urls = []
+        for url in [url['expanded_url'] for url in tweet['entities']['urls']]:
+            try:
+                r = requests.get(url,allow_redirects=True)
+                real_url = re.search("://(.+?)/",r.url).group(1)
+                urls.append(real_url)
+            except:
+                continue
+        utc_offset = str(tweet['user']['utc_offset'])
+        mentions = [ m['screen_name'] for m in tweet['entities']['user_mentions']]
+        line = user + "\t"
+        line += accounts[user]['state'] + '\t'
+        line += accounts[user]['party'] + "\t"
+        line += accounts[user]['chamber'] + "\t"
+        line += ",".join(mentions) + "\t"
+        line += ",".join(urls) + "\t"
+        line += tweet_time + "\t"
+        line += utc_offset + "\t"
+        line += tweet_txt + '\n'
+        file.write(line.encode("UTF-8"))
+
+    print time.time()- timea
 file.close()
 
