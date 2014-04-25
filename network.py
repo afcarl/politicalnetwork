560f820a32ca9fa239445f7b6925e1ba9fab8397
Matias <rasmussen.matias@gmail.com>
2014-04-25 23:05:26 +0200

    network of mentioned sites added

--- network.py
+++ network.py
@@ -1,6 +1,9 @@
 
 import networkx as nx
 
+g = nx.DiGraph()
+
+
 class xdict(dict):
 	def add(self,e):
 		if e in self:
@@ -10,14 +13,24 @@ class xdict(dict):
 
 
 names = xdict()
-freqs = xdict()
+sites = xdict()
 
 for line in file('tweets.tsv'):
 	parts = line.split("\t")
-	freqs.add( len(parts) )
-		
-print freqs
+	name = parts[0]
+	mentioned_sites = parts[5].split(",")
+	for site in mentioned_sites:
+		if name in g and site in g[name]:
+			g[name][site]['weight'] += 1
+		else:
+			g.add_edge(name,site,weight=1)
 
+for e in g.edges():
+	a,b = e
+	print e, g[a][b]
+		
+
+nx.write_graphml(g,"sites.graphml")
 
 
 
